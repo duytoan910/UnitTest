0a75a80666e6f5fdfd0305da1aaf80dceb7d8f96
Admin <Admin@H71M20>
2021-05-08 13:59:10 +0700

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int MaxAndMean(int A, int B,int C, out double Mean)
+        {
+            Mean = (A + B + C) / 3;
+            int Max;
+            if (A > B)
+            {
+                if (A > C)
+                    Max = A;
+                else Max = B;
+            }
+            else if (B > C) Max = B;
+            else Max = C;
+            return Max;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MaxAndMeanTest1()
+        {
+            Class1 cls = new Class1();
+            double Mean = 0.0;
+
+            int Exp_Max = 6;
+            double Exp_Mean = 4;
+
+            int max = cls.MaxAndMean(4, 6, 2, out Mean);
+
+            Assert.AreEqual(Exp_Max, max, 0.1);
+            Assert.AreEqual(Exp_Mean, Mean, 0.1);
+        }
+        [TestMethod]
+        public void MaxAndMeanTest2()
+        {
+            Class1 cls = new Class1();
+            double Mean = 0.0;
+
+            int Exp_Max = 6;
+            double Exp_Mean = 3;
+
+            int max = cls.MaxAndMean(4, 6, 2, out Mean);
+
+            Assert.AreEqual(Exp_Max, max, 0.1);
+            Assert.AreEqual(Exp_Mean, Mean, 0.1);
+        }
+    }
+}
